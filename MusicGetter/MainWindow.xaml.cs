725605419f0c522020057e55d0053b924693b7dc
Wouter Kenis <Da.wouter@gmail.com>
2018-11-27 10:46:13 +0100

    Add project files.

---
+++ MusicGetter/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace MusicGetter
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<MusicFile> musicFiles = new List<MusicFile>();
+
+            string myMusicPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+            CustomTitle = "Listing music for " + myMusicPath;
+            DataContext = this;
+
+            string[] allMusicFiles = Directory.GetFiles(myMusicPath, "*.mp3*");
+
+            foreach (string musicFile in allMusicFiles)
+            {
+                var file = TagLib.File.Create(musicFile);
+
+                musicFiles.Add(new MusicFile
+                {
+                    Artist = file.Tag.FirstAlbumArtist,
+                    Year = file.Tag.Year,
+                    Genre = file.Tag.FirstGenre,
+                    Title = file.Tag.Title
+                });
+            }
+
+            musicFileDataGrid.ItemsSource = musicFiles;
+        }
+
+        public string CustomTitle { get; set; }
+    }
+}
---
+++ MusicGetter/MusicFile.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicGetter
+{
+    public class MusicFile
+    {
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public uint Year { get; set; }
+    }
+}
